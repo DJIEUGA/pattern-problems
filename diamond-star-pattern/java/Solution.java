f52fe088d505b401daf693530a85f11de532f6f5
Djieuga Djoubi <djieugadjoubirylenathan@gmail.com>
2024-11-11 00:47:36 +0100

    added /diamond-star-pattern

---
+++ diamond-star-pattern/java/Solution.java
@@ -0,0 +1,39 @@
+class Solution {
+    static void pattern(int N) {
+        for (int r = 1; r <= N; r++) {
+            // print empty spaces before the stars
+            for (int c = 1; c <= N - r; c++) {
+                System.out.print(" ");
+            }
+            // print the stars
+            for (int c = 1; c <= 2 * r - 1; c++) {
+                System.out.print("*");
+            }
+            // print empty spaces after the stars
+            for (int c = 1; c <= N - r; c++) {
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+        for (int r = 1; r <= N; r++) {
+            // print empty spaces before the stars
+            for (int c = 1; c <= r - 1; c++) {
+                System.out.print(" ");
+            }
+            // print the stars
+            for (int c = (2 * N - r) + 1; c > r; c--) {
+                System.out.print("*");
+            }
+            // print empty spaces after the stars
+            for (int c = 1; c <= r - 1; c++) {
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        int n = 5;
+        pattern(n);
+    }
+}
